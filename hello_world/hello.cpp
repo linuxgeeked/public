ce3de933b0020a9e3d87528882282dcebd707c8a
builder1_mirror <Builder1@noreply>
2023-10-30 08:59:01 -0500

    Adding for L&L

---
+++ hello_world/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    // Print Hello, World!
+    std::cout << "Hello, World!" << std::endl;
+
+    // Count from 1 to 100
+    for (int i = 1; i <= 100; ++i) {
+        std::cout << i << std::endl;
+    }
+
+    return 0;
+}
+
